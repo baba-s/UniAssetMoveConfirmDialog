647c3822a6fbc4538546f82921cfa4c80c264bbf
baba_s <shotababa1206@gmail.com>
2020-10-07 20:27:36 +0900

    first commit

---
+++ Editor/AssetMoveConfirmDialog.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEditor;
+
+namespace Kogane.Internal
+{
+	internal sealed class AssetMoveConfirmDialog : UnityEditor.AssetModificationProcessor
+	{
+		private static bool m_isAllMove;
+
+		private static AssetMoveResult OnWillMoveAsset
+		(
+			string sourcePath,
+			string destinationPath
+		)
+		{
+			var settings = AssetMoveConfirmDialogSettings.GetInstance();
+
+			if ( !settings.Enabled ) return AssetMoveResult.DidNotMove;
+			if ( m_isAllMove ) return AssetMoveResult.DidNotMove;
+
+			var index = EditorUtility.DisplayDialogComplex
+			(
+				title: "UniAssetMoveConfirmDialog",
+				message: $"「{sourcePath}」を「{destinationPath}」に移動しますか？",
+				ok: "はい",
+				cancel: "いいえ",
+				alt: "すべて移動"
+			);
+
+			switch ( index )
+			{
+				case 0: return AssetMoveResult.DidNotMove;
+				case 1: return AssetMoveResult.FailedMove;
+				case 2:
+				{
+					m_isAllMove                 =  true;
+					EditorApplication.delayCall += () => m_isAllMove = false;
+					return AssetMoveResult.DidNotMove;
+				}
+			}
+
+			throw new InvalidOperationException();
+		}
+	}
+}
---
+++ Editor/AssetMoveConfirmDialogSettings.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+	internal sealed class AssetMoveConfirmDialogSettings :
+		ScriptableObjectForPreferences<AssetMoveConfirmDialogSettings>
+	{
+		[SerializeField] private bool m_enabled = false;
+
+		public bool Enabled => m_enabled;
+
+		[SettingsProvider]
+		private static SettingsProvider SettingsProvider()
+		{
+			return CreateSettingsProvider( "Kogane/UniAssetMoveConfirmDialog" );
+		}
+	}
+}
